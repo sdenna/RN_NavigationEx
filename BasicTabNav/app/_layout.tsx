e6031de185c980d3b4ddfbe33114787efe63a2e9
Shannon M. Denna <sdenna@d211.org>
2025-09-21 15:52:44 -0500

    Updated all example apps to SDK 54

--- BasicTabNav/app/_layout.tsx
+++
@@ -1,7 +0,0 @@
-import { Slot } from "expo-router";
-
-export default function RootLayout() {
-  return (
-    <Slot /> // Loads the first available screen (which is (tabs)/_layout.tsx)
-  );
-}
---
+++ NestedNavPassData_2025/app/index.tsx
@@ -0,0 +1,48 @@
+import colors from "@/styles/colors";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Button from "../components/Button";
+import TextField from "../components/TextField";
+
+export default function IndexScreen() {
+  const router = useRouter();
+  const [name, setName] = useState<string>("");
+  const [activity, setActivity] = useState<string>("");
+
+  const openTabNav = () => {
+    if (name != "" && activity != "")
+      router.push({ pathname: "/(tabs)", params: { name, activity } });
+    else alert("Please enter your name and activity");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to the App</Text>
+      <TextField placeholder="Name" value={name} onChangeText={setName} />
+
+      <TextField
+        placeholder="Activity"
+        value={activity}
+        onChangeText={setActivity}
+      />
+
+      <Button title="Continue" onPress={openTabNav} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.darkbackground,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: colors.textondark,
+    marginBottom: 20,
+  },
+});
